7fba86c1b80ef429f798ca82d9d9cb8ce2cc6698
f1nd1s <kuzubaev20012006@gmail.com>
2024-10-20 23:31:53 +0500

    a

--- src/RideContext.java
+++ src/RideContext.java
@@ -6,18 +6,6 @@ public class RideContext {
         this.fareStrategy = strategy;
     }
 
-    public void setFareStrategy(FareStrategy strategy) {
-        this.fareStrategy = strategy;
-    }
-
-    public void selectStrategyBasedOnTime(int hour) {
-        if (hour >= 7 && hour <= 10 || hour >= 17 && hour <= 20) {
-            setFareStrategy(new SurgeFareStrategy());
-        } else {
-            setFareStrategy(new RegularFareStrategy());
-        }
-    }
-
     public double calculateFare(double distance, double duration) {
         if (distance < 0 || duration < 0) {
             throw new IllegalArgumentException("Distance and duration must be non-negative.");
